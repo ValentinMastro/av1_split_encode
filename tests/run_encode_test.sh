aa1b3e6631eeab68396a40196839675e31f4d930
Valentin Mastro <valentinmastro@gmail.com>
2021-06-06 12:11:40 +0200

    run_encode_test.sh has to be runned from the main directory

--- tests/run_encode_test.sh
+++ tests/run_encode_test.sh
@@ -10,12 +10,14 @@ fi
 rm -rf encoded
 mkdir -p encoded
 N=0
-FFMPEG="$PWD/../ffmpeg_build/bin/ffmpeg"
 
-for file in sources/*
+
+FFMPEG="./ffmpeg_build/bin/ffmpeg"
+
+for file in tests/sources/*
 do
 	N=$(($N+1))
-	python ../start_encode.py "$PWD/$file" "encoded/$N.mkv" -q 48 --threads_per_split 1 --cpu_use 9 --clean_at_the_end --split_method 'Vapoursynth'
+	python ./start_encode.py "$PWD/$file" "tests/encoded/$N.mkv" -q 48 --threads_per_split 1 --cpu_use 9 --clean_at_the_end --split_method 'Vapoursynth'
 
 	if [ $N -eq $MAX ]
 	then 

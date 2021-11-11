27fcec1cfc92701c3fa37b6d6470f18422f2819c
Valentin Mastro <valentinmastro@hotmail.fr>
2021-11-11 21:51:19 +0100

    Modifications fichiers d'installation des dépendances : vérification de l'existence d'executable

--- external_binaries/install.sh
+++ external_binaries/install.sh
@@ -4,9 +4,25 @@ export PREFIX="$PWD"
 mkdir -p "$PREFIX/src"
 export SRC="$PREFIX/src"
 
-rm -rf bin/* include/* lib/* share/* src/*
-rmdir bin include lib share src
-mkdir -p "$SRC"
+rm -rf bin include lib share src
+
+check_exec_existence () {
+    INSTALLED=1
+
+    for exec in cmake meson ninja git make nasm autoreconf aclocal cython
+    do
+        if ! type "$exec" > /dev/null 2>&1
+        then
+            echo "$exec not installed on the system"
+            INSTALLED=0
+        fi
+    done
+
+    if [ "$INSTALLED" -eq 0 ]
+    then
+        exit 1
+    fi
+}
 
 compile_vmaf () {
 	# Download
@@ -20,6 +36,7 @@ compile_vmaf () {
 	mkdir -p "$PREFIX/share"
 	cd "$PREFIX/src/vmaf"
 	cp -R -P -p model "$PREFIX/share"
+	cd "$PREFIX"
 }
 
 compile_dav1d () {
@@ -30,6 +47,7 @@ compile_dav1d () {
 	cd "$PREFIX/src/dav1d"
 	meson --prefix="$PREFIX" --libdir lib --buildtype release -Dc_args="-O3 -march=native" -Denable_tools=false build
 	ninja -C build install
+	cd "$PREFIX"
 }
 
 compile_aomenc () {
@@ -44,6 +62,7 @@ compile_aomenc () {
 	cmake -DCMAKE_INSTALL_PREFIX="$PREFIX" -DBUILD_SHARED_LIBS=0 -DCONFIG_TUNE_VMAF=0 -DCMAKE_BUILD_TYPE=Release -DENABLE_EXAMPLES=1 -DENABLE_DOCS=0 -DENABLE_TESTS=0 -DCMAKE_CXX_FLAGS="-O3 -march=native" -DCMAKE_C_FLAGS="-O3 -march=native" -DCMAKE_C_FLAGS_INIT="-static" ..
 	cmake --build . -j
 	cmake --install .
+	cd "$PREFIX"
 }
 
 compile_svt_av1 () {
@@ -58,6 +77,7 @@ compile_svt_av1 () {
 	cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_BUILD_TYPE=Release -DBUILD_DEC=OFF -DBUILD_SHARED_LIBS=OFF ..
 	make -j
 	make install
+	cd "$PREFIX"
 }
 
 compile_ffmpeg () {
@@ -70,7 +90,7 @@ compile_ffmpeg () {
 		--extra-ldflags="-L $PREFIX/lib -Wl,-rpath,$PREFIX/lib"         \
 		--enable-gpl --enable-nonfree --disable-doc                     \
 		                                                                \
-		--enable-openssl --enable-libass --enable-libbluray             \
+        --enable-libass --enable-libbluray             \
 		--enable-libfreetype --enable-libfribidi                        \
 		--enable-libfontconfig                                          \
 		                                                                \
@@ -85,6 +105,7 @@ compile_ffmpeg () {
 
 	make -j
 	make install
+	cd "$PREFIX"
 }
 
 compile_vapoursynth () {
@@ -100,8 +121,13 @@ compile_vapoursynth () {
 	make install
 
 	cd "$PREFIX"
-	cp libvslsmashsource.so lib/vapoursynth/libvslsmashsource.so
+    mkdir -p "$PREFIX/lib/vapoursynth"
+	cp "$PREFIX/libvslsmashsource.so" "$PREFIX/lib/vapoursynth/libvslsmashsource.so"
 }
+
+
+check_exec_existence
+mkdir -p "$SRC"
 
 compile_vmaf
 compile_dav1d
